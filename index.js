81868debd2b2c82ce4101bce0ed0090a274df725
Artem Dergunov <lutaev.stepan@yandex.ru>
2019-08-25 19:10:21 +0300

    Add lib and travis file

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
 const Koa = require('koa');
+
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const readFileThunk = async src => {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(src, {'encoding': 'utf8'}, function (err, data) {
+      if(err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
+
+app.use(async (ctx, next) => {
+  await next();
+  ctx.body = await readFileThunk(__dirname + '/index.html');
 });
 
 app.listen(3000);
